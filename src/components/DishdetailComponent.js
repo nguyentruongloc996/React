e9f86b416a03730ec5b5f4364d9425e5c8f3e3b9
Truong Loc <nguyentruongloc996@gmail.com>
2022-05-01 22:06:45 +0700

    Comment Modal added

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,56 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Form, Modal } from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Form, Modal, Row, Col, Label, ModalHeader, ModalBody } from "reactstrap";
+import Select from 'react-select';
 import { Link } from "react-router-dom";
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+function CommentForm(props) {
+    const [modal, setModal] = React.useState(false);
+
+    const toggle = () => setModal(!modal);
+
+    const handleSubmit = (values) => {
+        console.log("Current State is: " + JSON.stringify(values));
+        alert("Current State is: " + JSON.stringify(values));
+    };
+
+    const ratingOptions = [
+        { value: 1, label: '1' },
+        { value: 2, label: '2' },
+        { value: 3, label: '3' },
+        { value: 4, label: '4' },
+        { value: 5, label: '5' },
+    ];
+
+    return (
+        <div >
+            <Button onClick={toggle} variant="outline-secondary" ><span className="fa fa-pencil"></span> Submit Comment</Button>
+            <div style={{display: 'block', width: 700, padding: 30}}>
+                <Modal
+                    isOpen={modal}
+                    toggle={toggle}
+                >
+                    <ModalHeader closeButton>
+                        Submit Comment
+                    </ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => handleSubmit(values)}>
+                            <Row className="form-group">
+                                <Col md={2}>
+                                    <Label htmlFor="rating">Rating</Label>                             
+                                </Col>
+                                <Col md={9}>
+                                    <Select options={ratingOptions} />
+                                </Col>
+                            </Row>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </div>
+        </div>
+        
+    );
+}
 
 
 function DishDetail(props) {
@@ -31,6 +81,13 @@ function DishDetail(props) {
         return date.toLocaleDateString("en-US", options);
     }
 
+    function showSubmitCommentForm() {
+        console.log("Try to open modal");
+        return (
+            <CommentForm />
+        );
+    }
+
     function renderComments(comments) {
         const contents = comments.map(comment => {
             return (
@@ -45,16 +102,8 @@ function DishDetail(props) {
             <div>
                 <h4>Comments</h4>
                 {contents}
-                <Form onSubmit={showSubmitCommentForm}>
-                    <Button variant="outline-secondary"><span className="fa fa-pencil"></span> Submit Comment</Button>
-                </Form>
+                <CommentForm />
             </div>
-        );
-    }
-
-    function showSubmitCommentForm() {
-        return (
-            <CommentForm />
         );
     }
 
@@ -86,19 +135,6 @@ function DishDetail(props) {
     else {
         return (
             <div></div>
-        );
-    }
-}
-
-class CommentForm extends React.Component {
-    constructor(props) {
-        super(props);
-
-    }
-
-    render() {
-        return (
-            <></>
         );
     }
 }
