d77f9cba743f714eb7da8f0dda1b5a7ce26c2f12
Truong Loc <nguyentruongloc996@gmail.com>
2022-06-19 16:16:20 +0700

    Adjust regex validation for email

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -8,7 +8,7 @@ const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
 const isNumber = (val) => !isNaN(Number(val));
-const validEmail = (val) => /^[A-Z0-9.%+-]+@[A-Z0-9.-]+\.[A-Z]{2.4}$/i.test(val);
+const validEmail = (val) => /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i.test(val);
 
 class Contact extends Component {
     constructor(props) {
